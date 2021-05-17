e7e41beffe45b3610b278b81b954c74dcd920f87
elowop <42976129+elowop@users.noreply.github.com>
2021-05-16 23:58:26 -0400

    working on row component
    add rowing component visual

---
+++ netflix-clone/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Row from './Components/Row'
+import requests from './requests'
+
+function App() {
+  return (
+    <div className="App">
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+    </div>
+  );
+}
+
+export default App;
