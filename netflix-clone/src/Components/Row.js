c30152a5ba1c5e595a4031bfcce48247b33d4ccd
elowop <42976129+elowop@users.noreply.github.com>
2021-06-06 14:38:25 -0400

    finalized netflix clone
    added in navigation bar, banner and completed row components. Added css to style website. Still need to host on firebase

--- netflix-clone/src/Components/Row.js
+++ netflix-clone/src/Components/Row.js
@@ -1,8 +1,22 @@
 import React, { useState, useEffect } from "react";
 import axios from '../axios';
+import "./Row.css"
+import Youtube from "react-youtube";
+import movieTrailer from 'movie-trailer';
 
-function Row({ title, fetchUrl }) {
+const baseImgUrl = "https://image.tmdb.org/t/p/original";
+
+function Row({ title, fetchUrl, isLargeRow}) {
   const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    },
+  };
 
   useEffect(() => {
     async function fetchData() {
@@ -13,13 +27,41 @@ function Row({ title, fetchUrl }) {
     fetchData();
   }, [fetchUrl]);
 
-  console.log(movies);
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  }
 
   return (
-    <div>
+    <div className="row">
       <h2>{title}</h2>
+
+      <div className="row_posters">
+        {movies.map((movie) => (
+              <img
+                key={movie.id}
+                onClick={() => handleClick(movie)}
+                className={`row_poster ${isLargeRow && "row_posterLarge"}`}
+                src={`${baseImgUrl}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                alt={movie.name}
+              />
+            )
+        )}
+      </div>
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
     </div>
   );
 }
+
 
 export default Row;
